543d0ca25e8cf892116a47182d2a734dcba42344
Artur <arturdemirchyan21@gmail.com>
2025-01-19 21:01:44 +0300

    ExceptionsLesson10

---
+++ ExceptionsLesson10/WrongLoginException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsLesson10
+{
+    public class WrongLoginException:Exception
+    {
+        private string _infoException;
+        public WrongLoginException() { }
+        public WrongLoginException(string infoException) :base(infoException)
+        {
+            _infoException = infoException;
+        }
+    }
+}
